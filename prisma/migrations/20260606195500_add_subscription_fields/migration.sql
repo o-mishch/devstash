786cf8a44907b3d7a3173e55c2e89778798c92dc
Aleksandr Mishchenko <aleksandr.mishchenko@dxc.com>
2026-06-07 01:32:21 +0200

    fix: correct relation name in subscription migration

--- prisma/migrations/20260606195500_add_subscription_fields/migration.sql
+++ prisma/migrations/20260606195500_add_subscription_fields/migration.sql
@@ -1,6 +1,6 @@
 -- CreateEnum
 CREATE TYPE "SubscriptionInterval" AS ENUM ('month', 'year');
 -- AlterTable
-ALTER TABLE "User"
+ALTER TABLE "users"
 ADD COLUMN "cancelAtPeriodEnd" BOOLEAN NOT NULL DEFAULT false,
     ADD COLUMN "subscriptionInterval" "SubscriptionInterval";
